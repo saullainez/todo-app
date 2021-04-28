4646989550b84069880041b853ecc67ee07c1df4
Saúl Laínez <saullainez@hotmail.es>
2021-04-28 08:29:17 -0600

    Crear el cascaron de la lista de TODOs

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -15,14 +15,52 @@ export const TodoApp = () => {
     return (
         <>
             
-            <h1>TodoApp</h1>
+            <h1>TodoApp ( { todos.length } )</h1>
             <hr />
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group list-group-flush">
+                        {
+                            todos.map( ( todo, i ) => (
+                                <li 
+                                    key={todo.id} 
+                                    className="list-group-item"
+                                > 
+                                    <p className="text-center">{ i +1 }. { todo.desc }</p> 
+                                    <button
+                                        className="btn btn-danger"
+                                    >
+                                        Borrar
+                                    </button>
+
+                                </li>
+                            ) )
+                        }
+                    </ul>
+                </div>
+                <div className="col-5">
+                        <h4>Agregar TODO</h4>
+                        <hr />
+                        <form>
+
+                            <input
+                                type="text"
+                                name="description" 
+                                placeholder="Aprender..."
+                                autoComplete="off"
+                                className="form-control"
+                            />
+
+                            <button
+                                className="btn btn-outline-primary mt-1 btn-block"
+                            >
+                                Agregar
+                            </button>
+
+                        </form>
+                </div>
+            </div>
 
         </>
     )

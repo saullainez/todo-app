35c284beb184bc023326d35203ff7f54d54a7d16
Saúl Laínez <saullainez@hotmail.es>
2021-04-28 10:53:24 -0600

    Refactorización
    Crear componentes:
     -TodoList
     -TodoListItem

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -2,6 +2,7 @@ import React, { useEffect, useReducer } from 'react';
 import { useForm } from '../hooks/useForm';
 import '../styles.css';
 import { todoReducer } from '../todoReducer';
+import { TodoList } from './TodoList';
 
 
 const init = () => {
@@ -69,30 +70,11 @@ export const TodoApp = () => {
 
             <div className="row">
                 <div className="col-7">
-                    <ul className="list-group list-group-flush">
-                        {
-                            todos.map( ( todo, i ) => (
-                                <li 
-                                    key={todo.id} 
-                                    className="list-group-item"
-                                > 
-                                    <p 
-                                        className={ `${ todo.done && 'completed' }` }
-                                        onClick={ () => handleToggle( todo.id ) }
-                                    >
-                                        { i +1 }. { todo.desc 
-                                    }</p> 
-                                    <button
-                                        className="btn btn-danger"
-                                        onClick={() => handleDelete(todo.id)}
-                                    >
-                                        Borrar
-                                    </button>
-
-                                </li>
-                            ) )
-                        }
-                    </ul>
+                    <TodoList 
+                        todos = { todos } 
+                        handleDelete = { handleDelete }
+                        handleToggle = { handleToggle }
+                    />
                 </div>
                 <div className="col-5">
                         <h4>Agregar TODO</h4>
